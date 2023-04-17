e0796d72a5980e5de1f6e0a33e720e01de2551cd
pankaj9695 <pankaj9695@yahoo.com>
2023-04-17 10:18:37 +0530

    code updated

--- app.js
+++ app.js
@@ -1,53 +1,127 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
 
+// Set up the MongoDB connection string
+mongoose.connect('mongodb+srv://pkrhtdm:987654321.0@hypermovegame.loejrx2.mongodb.net/HypermoveGame');
+
+// Create a schema for the GameStats collection
+const gameStatsSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+    unique: true
+  },
+  walletAddress: {
+    type: String,
+    unique: true
+  },
+  coinRewarded: Boolean,
+  numCoins: Number,
+  winCount: Number,
+  matchesCompleted: Boolean,
+  playMinit: Number,
+  lastRewardTime: Date
+});
+
+// Create a model for the GameStats collection
+const GameStats = mongoose.model('GameStats', gameStatsSchema, 'PlayersData');
+
+// Set up the API endpoints
 const app = express();
-app.use(express.json());
-app.use(cors());
+app.use(bodyParser.json());
 
-// Replace with your own MongoDB connection string
-const connectionString = 'mongodb+srv://pkrhtdm:987654321.0@hypermovegame.loejrx2.mongodb.net';
-mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
-    console.log("Connected");
-}).catch((e)=>{
-    console.log(e);
-});
+// Endpoint for saving wallet address
+app.post('/saveWalletAddress', async (req, res) => {
+  const { userId, walletAddress, coinRewarded, numCoins } = req.body;
 
-const walletSchema = new mongoose.Schema({
-    userId: String,
-    address: String,
-    coinsRewarded: Boolean,
-    numCoins: Number
-});
-
-const Wallet = mongoose.model('Wallet', walletSchema);
-
-app.get('/saveWalletAddress', async (req, res) => {
-    const walletAddress = req.query.walletAddress;
-    console.log('Received wallet address:', walletAddress); // Log the received wallet address
-    let wallet = await Wallet.findOne({ address: walletAddress });
-
-    if (!wallet) {
-        console.log('Creating a new wallet record'); // Log when creating a new wallet record
-         wallet = new Wallet({ userId: userId, address: walletAddress, coinsRewarded: false, numCoins: 0 });
-        await wallet.save();
-    } else {
-        console.log('Found an existing wallet record'); // Log when an existing wallet record is found
+  try {
+    // Check if the userId is already in the database
+    const existingUser = await GameStats.findOne({ userId });
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
     }
 
-    if (!wallet.coinsRewarded) {
-        console.log('Rewarding in-game coins'); // Log when rewarding in-game coins
-        wallet.coinsRewarded = true;
-        wallet.numCoins = 150;
-        await wallet.save();
-        res.send('150');
-    } else {
-        console.log('No reward: Wallet already rewarded'); // Log when the wallet has already been rewarded
-        res.send('0');
+    // Check if the wallet address is already in the database
+    const existingWallet = await GameStats.findOne({ walletAddress });
+    if (existingWallet) {
+      return res.status(400).json({ error: 'Wallet address already exists' });
     }
+
+    // Create a new game stats document for the user
+    const newGameStats = new GameStats({
+      userId,
+      walletAddress,
+      coinRewarded,
+      numCoins,
+      winCount: 0,
+      matchesCompleted: false,
+      playMinit: 0,
+      lastRewardTime: null
+    });
+
+    // Save the new game stats document to the database
+    await newGameStats.save();
+
+    // Return a success message
+    return res.status(200).json({ message: 'Wallet address saved successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+// Endpoint for tracking game stats
+app.post('/trackGameStats', async (req, res) => {
+  const { userId, playMinit } = req.body;
+
+  try {
+    // Find the existing game stats document for the user
+    const gameStats = await GameStats.findOne({ userId });
+
+    // If the user has no existing game stats document, return an error response
+    if (!gameStats) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Increment the player's win count
+    gameStats.winCount += 1;
+
+    // Check if the player has completed 5 matches
+    if (gameStats.winCount >= 5 && !gameStats.matchesCompleted) {
+      // Update the matchesCompleted flag and send a success response
+      gameStats.matchesCompleted = true;
+      await gameStats.save();
+      return res.status(200).json({ message: 'Matches completed', matchesCompleted: true });
+    }
+
+    // Update the player's playMinit variable
+    gameStats.playMinit += playMinit;
+
+    // Check if the player has played for at least 60 minutes since their last reward
+    if (gameStats.playMinit >= 60 && (!gameStats.lastRewardTime || new Date() - gameStats.lastRewardTime >= 86400000)) {
+      // Reward the player with 10 coins and update the lastRewardTime variable
+      gameStats.lastRewardTime = new Date();
+      gameStats.numCoins += 10;
+    }
+
+    // Save the updated game stats document to the database
+    await gameStats.save();
+
+    // Return a success message
+    return res.status(200).json({ message: 'Game stats updated successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+    
+    // Start the server
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+    
